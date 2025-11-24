a475be3f6aea3f131d7e5219a09bf8e9e30ec9c5
HN-01 <haseebnawaz181206@gmail.com>
2025-11-24 19:10:40 +0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD12UopySpTvHhNFKQ55Hepibbr2bd-iQs",
+  authDomain: "dlab-2f49f.firebaseapp.com",
+  projectId: "dlab-2f49f",
+  storageBucket: "dlab-2f49f.firebasestorage.app",
+  messagingSenderId: "211639635026",
+  appId: "1:211639635026:web:5eb95efc46a6e96a2fa38b",
+  measurementId: "G-R0RGTQXRZ1"
+};
